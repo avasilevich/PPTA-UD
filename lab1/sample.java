1ffbaff2f6b1dac8f2a8541351a148d37d9e096a
Uladzislau Danilchyk <danilchican@mail.ru>
2018-09-22 14:24:27 +0300

    Initial commit

---
+++ lab1/sample.java
@@ -0,0 +1,12 @@
+// single line comment
+public class ClassName {
+ /* 
+   class variable 
+ */
+ private Integer intVar = 10;
+
+ public func void main() {
+  Double doubVar = 10.2;
+  String strVar = "short string";
+ }
+}
