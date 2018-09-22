cd0e38e5dfb96d5dfeda51aa4737bddc2b48d9ce
Uladzislau Danilchyk <danilchican@mail.ru>
2018-09-22 15:02:34 +0300

    Modified parser and sample

--- lab1/sample.java
+++ lab1/sample.java
@@ -1,12 +1,32 @@
-// single line comment
-public class ClassName {
- /* 
-   class variable 
- */
- private Integer intVar = 10;
+package com.danilchican.ppta.lab1;
 
- public func void main() {
-  Double doubVar = 10.2;
-  String strVar = "short string";
- }
+public class ClassName {
+	/**
+	 * multi-line comment 
+	 * multi-line subcomment
+	 */
+	private static Integer intVar = 10; // single line comment
+	protected String myStrng = "....string....";
+
+	public void main() {
+		// define variable
+		Double doubVar = 10.2 + 10.5 - 1.2 / 2.0 * 1.0;
+		doubVar++;
+		doubVar--;
+
+		String strVar = "short string";
+		strVar += "substr";
+
+		doubVar -= 1.0;
+		doubVar *= 614.10;
+		doubVar /= 10.15;
+
+		if (a == b && 1 != 2 || 2 >= 0 && 1 <= 1) {
+			doubVar = 10;
+		} else {
+			doubVar = 11;
+		}
+
+		Integer mas[10] = {...};
+	}
 }
